31aee022db03e0f018f597123cb5847ede69629e
Rostislav <rvssv@bk.ru>
2022-07-30 13:51:48 +0300

    added new style for select

--- src/components/SelectWrapper.vue
+++ src/components/SelectWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-    <select>
+    <select class="select-wrapper">
         <option>По умолчанию</option>
         <option>Пункт 2</option>
     </select>
@@ -12,5 +12,15 @@
 </script>
 
 <style lang="scss">
-
+.select-wrapper {
+    width: 121.49px;
+    max-height: 36px;
+    font-size: $formFontSize;
+    color: $greyColor;
+    background: $formBackground;
+    border: none;
+    border-radius: $borderRadius;
+    box-shadow: $inputShadow;
+    text-align: center;
+}
 </style>
