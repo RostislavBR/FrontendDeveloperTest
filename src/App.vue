8e521555ee8ed3632afc52c0adff37e2a784b481
Rostislav <rvssv@bk.ru>
2022-07-30 11:50:13 +0300

    added new branch feature_header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header/>
     <main class="main">
       <div class="container">
         <router-view />
@@ -22,4 +23,25 @@
     margin: 0;
     padding: 0;
   }
+  body, html {
+    width: 100%;
+    height: 100%;
+    overflow-x: hidden;
+    background: $backgroundColor;
+  }
+  #app {
+    padding: 0 32px;
+  }
+  .container {
+    display: flex;
+    width: 1920px;
+    margin: 0 auto;
+  }
 </style>
+<script>
+  import Header from "@/components/Header";
+
+  export default {
+    components: { Header }
+  }
+</script>
