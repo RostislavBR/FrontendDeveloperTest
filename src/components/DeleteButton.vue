b542eee3c400e69984efae3fa524c8a41bff3bcf
Rostislav <rvssv@bk.ru>
2022-08-04 12:26:31 +0300

    added new branch animation_delete_button

--- src/components/DeleteButton.vue
+++ src/components/DeleteButton.vue
@@ -1,9 +1,10 @@
 <template>
-    <button class="delete-button" @click="clickButton"></button>
+    <button class="delete-button" @click="clickButton" ref="deleteButton"></button>
 </template>
 
 <script>
     import { mapMutations } from  "vuex";
+    import gsap from "gsap";
 
     export default {
         name: "DeleteButton",
@@ -11,7 +12,14 @@
         methods: {
             ...mapMutations({deleteProduct: "deleteProduct"}),
             clickButton() {
-                this.deleteProduct(this.id);
+                gsap.to(this.$parent.$el, {
+                    duration: .2,
+                    delay: .1,
+                    opacity: 0,
+                    onComplete:() => {
+                        this.deleteProduct(this.id);
+                    }
+                });
             }
         },
     }
@@ -30,5 +38,9 @@
         background-color: $pinkColor;
         border: none;
         border-radius: $buttonRadius;
+        opacity: 0;
+        pointer-events: none;
+        transition: opacity 0.4s ease;
+        cursor: pointer;
     }
 </style>
